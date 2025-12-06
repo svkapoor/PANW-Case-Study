f3fea96abb25a15ef716c6077e918aea24317e78
Samar Kapoor <svkapoor14@gmail.com>
2025-12-06 15:22:08 -0500

    Created sentiment analyzer with Vader

--- journal/sentiment.py
+++ journal/sentiment.py
@@ -1,3 +1,8 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Loads Vader's model
+analyzer = SentimentIntensityAnalyzer()
+
 def clean_text(text: str) -> str:
     """
     Basic cleaning for journaling text.
@@ -9,6 +14,38 @@ def clean_text(text: str) -> str:
     text = " ".join(text.split())
     return text
 
+def base_sentiment(text: str) -> str:
+    """
+    Use VADER to get basic sentiment classification.
+    Returns one of: 'positive', 'negative', 'neutral'.
+    """
+    scores = analyzer.polarity_scores(text)
+    compound = scores["compound"]  # summarizes positivity vs negativity
+
+    if compound >= 0.6:
+        return "very positive"
+    elif compound >= 0.35:
+        return "slightly positive"
+    elif compound <= -0.6:
+        return "very negative"
+    elif compound <= -0.35:
+        return "slightly negative"
+    else:
+        return "neutral"
+
+
+# Call to return sentiment of statement
+def analyze_sentiment(text: str) -> str:
+    # Clean
+    cleaned_txt = clean_text(text)
+    sentiment = base_sentiment(cleaned_txt)
+    return sentiment
+
+
+
 # Testing
 if __name__ == "__main__":
-    print(clean_text("Butterflys are    awesome.  "))
+    # Cleaning Test
+    statement = "Its raining crazy"
+    print(clean_text("Cleaned statement " + statement))
+    print("Sentiment: " + analyze_sentiment(statement))
