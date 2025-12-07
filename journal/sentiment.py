00956303d9277e231a4e6b2066bb06eca6b25aea
Samar Kapoor <svkapoor14@gmail.com>
2025-12-07 14:08:04 -0500

    using hf model

--- journal/sentiment.py
+++ journal/sentiment.py
@@ -3,17 +3,20 @@ from transformers import pipeline
 import os
 
 # Get the absolute path to the current script's directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
+# current_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct path to the model relative to this script
-model_path = os.path.join(current_dir, "..", "emotion_bert")
+# model_path = os.path.join(current_dir, "..", "training", "emotion_model")
+
+from transformers import logging
+logging.set_verbosity_error()
 
 emotion_analyzer = pipeline("text-classification",
-                    model=model_path,
-                    return_all_scores=False)
+                    model="svkapoor/emotion_model_distilbert-base-uncased",
+                    top_k=1)
 
 def analyze_sentiment(text):
     result = emotion_analyzer(text)
-    return result[0]['label']
+    return result[0][0]['label']
 
 if __name__ == "__main__":
-    print(analyze_sentiment("this lowky a pain"))
+    print(analyze_sentiment("ur an idiot"))
